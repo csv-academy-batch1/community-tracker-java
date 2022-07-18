34e7625bc74a97934776b54500dfef2c47fd2323
RestanJade16 <106858218+RestanJade16@users.noreply.github.com>
2022-07-18 16:47:38 +0800

    Delete src/main/java/com/community/Community/Management directory

--- src/main/java/com/community/Community/Management/service/CommunityManagerService.java
+++
@@ -1,9 +0,0 @@
-package com.community.Community.Management.service;
-
-import com.community.Community.Management.model.CommunityManager;
-
-public interface CommunityManagerService {
-
-    CommunityManager getCommunityManagerByID(int id);
-
-}
