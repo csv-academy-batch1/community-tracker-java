1a5d8fc9fd0f17e02a1056a85d0c7ee19972a607
Jerome Garcia <106857759+jeromecsv@users.noreply.github.com>
2022-07-21 13:26:01 +0800

    Delete data.sql

--- src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO community (communityname, communityicon, communitymgrid, communitydesc, isactive) VALUES ('Enterprise Coffee', 'null', 1, 'Java Developer Description', true);
-INSERT INTO community (communityname, communityicon, communitymgrid, communitydesc, isactive) VALUES ('Enterprise .NET', 'null', 2, 'Enterprise .NET Description ', true);
-INSERT INTO community (communityname, communityicon, communitymgrid, communitydesc, isactive) VALUES ('Mobile Cross Platform', 'null', 3, 'Mobile Cross Platform Description', false);
-
-INSERT INTO communityadminandmanager (communityadminandmanagername, csvemail, passkey, roletype, isactive) VALUES ('Jerome Garcia', 'jerome.garcia@softvision.com','password', 'admin', true);
-INSERT INTO communityadminandmanager (communityadminandmanagername, csvemail, passkey, roletype, isactive) VALUES ('Restan Jade Fernandez', 'restanjade.fernandez@softvision.com','password','admin',true);
-INSERT INTO communityadminandmanager (communityadminandmanagername, csvemail, passkey, roletype, isactive) VALUES ('Jay Transfiguration', 'jay.transfiguration@softvision.com','password','admin',true);
