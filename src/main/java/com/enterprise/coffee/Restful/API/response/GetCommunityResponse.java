a2309636c19aeca4ba2760cdeee24c3833c40245
restan.fernandez <restanfernandez16@gmail.com>
2022-07-21 17:50:21 +0800

    PascalCase

--- src/main/java/com/enterprise/coffee/Restful/API/response/GetCommunityResponse.java
+++ src/main/java/com/enterprise/coffee/Restful/API/response/GetCommunityResponse.java
@@ -7,11 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 @Data
 public class GetCommunityResponse {
-<<<<<<< HEAD:src/main/java/com/enterprise/coffee/Restful/API/response/GetCommunityResponse.java
-
     @JsonProperty("Communities")
-=======
->>>>>>> fdade3b8756a36682f2a6720223c549f39d010ac:src/main/java/com/enterprise/coffee/Restful/API/model/GetCommunityResponse.java
     List<CommunityList> communities = new ArrayList<>();
 }
 
